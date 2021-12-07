3e83469ea6df6330d15318e2473d334b0296df8e
Arnab Dutta <95521825+ArnabDuttaWebDeveloper111@users.noreply.github.com>
2021-12-08 02:30:26 +0600

    Add files via upload

---
+++ partials/dbconnect.php
@@ -0,0 +1,15 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "users";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+
+// if ($conn) {
+//     echo "Success";
+// } else {
+//     die("Error" . mysqli_connect_errno());
+// }
